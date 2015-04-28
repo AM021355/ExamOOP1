b626c811339844f5dcd77c90dc5193c3718e60cf
registermark3 <registermark3@gmail.com>
2015-04-28 16:54:57 +0800

    C revert B

--- Question1.java
+++ Question1.java
@@ -1,6 +1,10 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
+		System.out.println("A");
 		System.out.println("Test");
+<<<<<<< HEAD
 		System.out.println("B"); 
+=======
+>>>>>>> parent of 5d7894d... A AM021352 陳仕勳提交
 	}
 }
