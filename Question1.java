531467ea86ba70dd2624fda9c02e789d0887a2fa
registermark3 <registermark3@gmail.com>
2015-04-28 16:56:13 +0800

    C AM021327 吳威融提交

--- Question1.java
+++ Question1.java
@@ -2,9 +2,9 @@
 	public statis void main(String[] args){
 		System.out.println("A");
 		System.out.println("Test");
-<<<<<<< HEAD
+
 		System.out.println("B"); 
-=======
->>>>>>> parent of 5d7894d... A AM021352 陳仕勳提交
+		System.out.println("C"); 
+
 	}
 }
